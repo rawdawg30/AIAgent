5c39efab0031bbfefb9b9ba0beb24250ffd5b71d
rawdawg30 <swrawdon@gmail.com>
2026-02-19 14:44:14 -0800

    created write_file get_file_content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,7 +1,32 @@
+import os
+#import sys
+#sys.path.append('../')
 
+#from AIAGENT import MAX_CHARS
 def get_file_content(working_directory, file_path):
-    # return(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
-    # if file path is outside working directory
+    try:
+        # return(f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
+        # if file path is outside working directory
+        abs_path = os.path.abspath(working_directory)
+        full_path = os.path.normpath(os.path.join(abs_path, file_path))
+        valid_target_dir = os.path.commonpath([abs_path, full_path]) == abs_path
+        if not valid_target_dir:
+                return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
 
-    #if file not a file
-    #return(f'Error: File not found or is not a regular file: "{file_path}"')
+        #if file not a file
+        #return(f'Error: File not found or is not a regular file: "{file_path}"')
+        if not (os.path.isfile(full_path)):
+            return f'Error: File nor found or is not a regular file: "{file_path}"'
+        #read file and return as string
+        # use .read() with 10000 as limit on chars
+        return_str = ""
+        with open(full_path) as f:  
+            return_str =  f.read(10000)
+            if f.read(1):
+                return_str += f'[...File "{file_path}" truncated at 10000 characters]'
+            
+        return return_str
+    
+    except Exception as e:
+         return f"Error: {e}"
+        
---
+++ functions/write_file.py
@@ -0,0 +1,20 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        abs_path = os.path.abspath(working_directory)
+        full_path = os.path.normpath(os.path.join(abs_path, file_path))
+        valid_target_dir = os.path.commonpath([abs_path, full_path]) == abs_path
+        if not valid_target_dir:
+            return f'Error: Cannot write to "{file_path} as it is outside the permitted working directory'
+        if os.path.isdir(full_path):
+            return f'Error: Cannot write to "{file_path}" as it is a directory'
+        os.makedirs(os.path.dirname(full_path), exist_ok=True)
+
+        with open(full_path, 'w') as f:
+            f.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    
+    except Exception as e:
+        return f"Error: {e}"
